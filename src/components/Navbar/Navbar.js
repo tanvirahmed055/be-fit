8d3987d25b875390f605a3f2df16b81a586314b7
tanvirahmed055 <ahmedtanvir6168@gmail.com>
2021-10-04 19:57:31 +0600

    added NavLink

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -4,7 +4,7 @@ const Navbar = () => {
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
             <div className="container-fluid">
-                <a className="navbar-brand ms-5 fs-1 fw-bolder" href="#">BeFit</a>
+                <NavLink to="/" className="navbar-brand ms-5 fs-1 fw-bolder" href="#">BeFit</NavLink>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>

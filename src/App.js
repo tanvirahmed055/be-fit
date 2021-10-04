b3a3e72f2b6b4a4a538ca11e6bf57aec29d4fe47
tanvirahmed055 <ahmedtanvir6168@gmail.com>
2021-10-04 23:57:21 +0600

    added commenting

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
-
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-
 import Home from "./components/Home/Home";
-
 import About from "./components/About/About";
 import Services from "./components/Services/Services";
 import Contact from "./components/Contact/Contact";
 import Footer from "./components/Footer/Footer";
 import Navbar from "./components/Navbar/Navbar";
-
 import NotFound from "./components/NotFound/NotFound";
 import './App.css';
 
@@ -18,7 +14,6 @@ function App() {
       <Router>
         <Navbar></Navbar>
         <Switch>
-
           <Route exact path="/">
             <Home></Home>
           </Route>

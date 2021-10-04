74f2de06f4bf21845d9513976ce5b6fec6111d72
tanvirahmed055 <ahmedtanvir6168@gmail.com>
2021-10-04 19:01:02 +0600

    made change in contact component

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -5,7 +5,10 @@ import './Contact.css';
 const Contact = () => {
     return (
         <div className="my-5">
-            <h1 className="fw-bold">Get in touch with us</h1>
+            <h1 className="fw-bold">Get in touch</h1>
+
+            <p>Want to get in touch? We'd love to hear from you. Here's how you can reach us...</p>
+
             <hr className="horizontal-line" />
             <ContactForm></ContactForm>
         </div>

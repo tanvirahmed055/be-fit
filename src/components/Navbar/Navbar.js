20aa5e4d0e10969bd984f91e3a844a86f06fcaf5
tanvirahmed055 <ahmedtanvir6168@gmail.com>
2021-10-04 23:15:59 +0600

    made change in NavLink active style

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -11,16 +11,26 @@ const Navbar = () => {
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
                         <li className="nav-item me-5">
-                            <NavLink to="/home" className="nav-link" aria-current="page" >Home</NavLink>
+                            <NavLink to="/home" className="nav-link text-white" activeStyle={{
+                                fontWeight: "bold"
+                            }} >Home</NavLink>
                         </li>
                         <li className="nav-item me-5">
-                            <NavLink to="/about" className="nav-link" >About</NavLink>
+                            <NavLink to="/about" className="nav-link text-white" activeStyle={{
+                                fontWeight: "bold"
+                            }}>About</NavLink>
                         </li>
                         <li className="nav-item me-5">
-                            <NavLink to="/services" className="nav-link" >Services</NavLink>
+                            <NavLink to="/services" className="nav-link text-white"
+                                activeStyle={{
+                                    fontWeight: "bold"
+                                }}>Services</NavLink>
                         </li>
                         <li className="nav-item me-5">
-                            <NavLink to="contact" className="nav-link" >Contact</NavLink>
+                            <NavLink to="contact" className="nav-link text-white"
+                                activeStyle={{
+                                    fontWeight: "bold"
+                                }}>Contact</NavLink>
                         </li>
                     </ul>
                 </div>

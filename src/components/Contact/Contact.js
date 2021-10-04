a0101c5548edfebd1ee9800d49f5e483d9ea86a4
tanvirahmed055 <ahmedtanvir6168@gmail.com>
2021-10-04 18:52:46 +0600

    added contact component

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import ContactForm from "./../ContactForm/ContactForm";
+import './Contact.css';
 
 const Contact = () => {
     return (
-        <div>
-
+        <div className="my-5">
+            <h1 className="fw-bold">Get in touch with us</h1>
+            <hr className="horizontal-line" />
+            <ContactForm></ContactForm>
         </div>
     );
 };

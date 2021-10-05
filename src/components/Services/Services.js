980722f0f9fd8654ee71e714e48dab255f4fbd61
tanvirahmed055 <ahmedtanvir6168@gmail.com>
2021-10-05 23:16:23 +0600

    made minor style changes

--- src/components/Services/Services.js
+++ src/components/Services/Services.js
@@ -20,7 +20,8 @@ const Services = () => {
     return (
         <div>
             <div className="container">
-                <h1 className="mt-4 mb-4 text-success fw-bolder">All Premium Services</h1>
+                <h1 className="mt-5 mb-3 text-primary fw-bolder service-title">All Premium Services</h1>
+                <hr className="mb-5 services-horizontal-line text-warning" />
                 <div className="row">
                     <div className="col-md-12 col-lg-12 col-sm-12 mb-5 ">
                         <div className="row services-container">
